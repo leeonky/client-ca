a8f8e0a92b776ed457f74d55718ad610f7d2780e
leeonky <leeonky@gmail.com>
2019-05-07 05:13:27 +0000

    Update scrpit

--- create-user.sh
+++ create-user.sh
@@ -7,7 +7,6 @@ cd $username
 openssl req -new -newkey rsa:4096 -nodes \
     -keyout user.key -out user.csr
 
-openssl pkcs12 -export -out user.pfx -inkey user.key -in user.crt -certfile ../ca.crt
 openssl x509 -req -days 365 -in user.csr -CA ../ca.crt -CAkey ../ca.key -set_serial 01 -out user.crt
 openssl pkcs12 -export -out user.pfx -inkey user.key -in user.crt -certfile ../ca.crt
 
