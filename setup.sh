19ec2b58ba20885d43f7f7809a1c84b130253d95
leeonky <leeonky@gmail.com>
2019-10-16 13:07:37 +0800

    Auto creat rnd file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 set -u
 . .env
+[ -e ~/.rnd ] || openssl rand -writerand ~/.rnd
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
     -subj "/C=$COUNTRY/ST=$STATE/L=$LOCATION/O=$ORGANIZATION/CN=$COMMON_NAME" \
     -keyout ca.key  -out ca.crt
